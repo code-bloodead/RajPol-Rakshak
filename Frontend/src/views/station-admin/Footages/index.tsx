4eafd07b8c701d563a9c50039c543c8ff2db0d2a
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-12-27 10:39:35 +0530

    CCTV selection
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- Frontend/src/views/station-admin/Footages/index.tsx
+++ Frontend/src/views/station-admin/Footages/index.tsx
@@ -66,7 +66,7 @@ const Footages = () => {
             display: activeTab === Tab.Footage ? "grid" : "none",
           }}
         >
-          {streams.map((stream, idx) => (
+          {streams.map((stream) => (
             <Card extra="min-h-[40vh] ">
               <div className="flex items-center justify-start">
                 <h1 className="ml-3 mt-3 text-xl font-bold text-navy-700 dark:text-white">
@@ -74,9 +74,21 @@ const Footages = () => {
                 </h1>
                 <div className="grow"></div>
                 <div className="flex justify-end mx-2 my-2 mt-4 me-4">
-                  <span className="mr-2 rounded px-2.5 py-0.5 text-xs font-medium bg-red-100 border border-red-400 text-red-700">
+                  <div className="mr-2 rounded px-2.5 py-0.5 text-xs font-medium bg-red-100 border border-red-400 text-red-700">
                     Fight alert
-                  </span>
+                  </div>
+                  <label htmlFor="underline_select" className="sr-only">
+                    Select CCTV
+                  </label>
+                  <select
+                    id="countries"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    <option value="CCTV-1">CCTV-1</option>
+                    <option value="CCTV-2">CCTV-2</option>
+                    <option value="CCTV-3">CCTV-3</option>
+                    <option value="CCTV-4">CCTV-4</option>
+                  </select>
                 </div>
               </div>
               <div className=" p-2 mt-1">
